af6ab7230511c2b253f200020935782ddf4336fa
源殇 <123342040+yuanshang000@users.noreply.github.com>
2025-05-02 15:19:33 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const API_KEYS = process.env.API_KEYS ?
   process.env.API_KEYS.split(',').map(key => key.trim()).filter(Boolean) :
   [];
-const BASE_URL = 'https://free.yunwu.ai';
+// const BASE_URL = process.env.BASE_URL || 'https://api.openai.com';
+
+const BASE_URL = process.env.BASE_URL || 'https://api.openai.com';
 
 let currentKeyIndex = 0;
 
